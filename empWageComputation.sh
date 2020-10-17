32a01e53a1319ed088f0efb3622e29e12d597756
PratibhaMastud <67584081+PratibhaMastud@users.noreply.github.com>
2020-10-17 23:58:04 +0530

    Added:Check Employee is Present or Not.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,10 @@
 #Welcome to Employee Wage Computation.
+#Author:PratibhaMastud
+#! /bin/bash
+R=$(( RANDOM%2 ))
+if [ $R -eq 1 ]
+then
+        echo "Employee is Present"
+else
+        echo "Employee is Not Present"
+fi
