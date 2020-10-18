3fb2500eecbea854f2d5ba50fb6c9efbdb08659e
PratibhaMastud <karandepratibha999@gmail.com>
2020-10-19 01:26:58 +0530

    Refactor : Calculate Working Hour Based on Working Day

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,19 +1,15 @@
 #! /bin/bash
-#Author:Pratibha Mastud
-#Calculate Wage till condition of total working hour
 
-wagePerHour=20
-workingDay=20
-fulldayHour=8
-totalHourForMonth=100
-totalWages=0
-a=0
+function workHour(){
+	var1=$i
+	
+	workingHour=$(($fullDayHour*$var1))
 
+	echo "Working Hour is = $workingHour"
+}
+fullDayHour=8
 for (( i=1; i<=20; i++ ))
-do	
-	perDayWages=$(($fulldayHour*$wagePerHour))
-	wageRespectiveDays=$(($perDayWages*i))
-	totalWages=$(($wageRespectiveDays+$totalWages))
-	a=$(($a+$i))
-	echo "Day : $i" "      $i-Day Wage = $wageRespectiveDays   " "  $a-Day total wage is $totalWages"
+do
+	echo "Working Day :" $i
+	workHour $i
 done
