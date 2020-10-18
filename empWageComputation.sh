f270f7e3c22cb561922759c09a00521a39d767b9
PratibhaMastud <karandepratibha999@gmail.com>
2020-10-19 00:01:47 +0530

    Added:Calculate Wage for Month.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,48 +1,11 @@
-#! /bin/bash -x
+#! /bin/bash
 #Author:Pratibha Mastud
-#Using Switch Case : 1.To check Employee Present or not. or 2.Calculate Daily Employee Wage. or 3.Add Part Time Employee and wage
+#Calculate Wage for Month
 
-echo "1.To check Employee Present or not. or 2.Calculate Daily Employee Wage. or 3.Add Part Time Employee and wage"
-read num
+wagePerHour=20
+workingDay=20
+fulldayHour=8
+totalHourForMonth=$(($workingDay*$fulldayHour))
 
-case $num in
-        1)
-                echo "Case1 : is to check Employee is Present or Absent."
-                if [ $num -eq 1 ]
-                then
-                    echo "Employee is Present."
-                 else
-                        echo "Employee is Absent."
-                fi
-                ;;
-        2)
-               echo "Case2 : is Calculate Daily Employee Wage."
-                wagePerHour=20
-                oneDayHour=8
-                dailyWageEmployee=$(($wagePerHour*$oneDayHour))
-
-                echo "Daily Employee Wage : $dailyWageEmployee"
-                ;;
-        3)
-                echo "Case3 : is Add Part time Employee and there wage."
-                Emp=0
-                wagePerHour=20
-                Addition=0
-                for (( i=0; i<5; i++ ))
-                do
-                        R=$((RANDOM%2))
-                        if [ $R -eq 1 ]
-                        then
-                                Emp=$(($Emp+1))
-                                Add=$(($wagePerHour*8))
-                                Addition=$(($Add*$Emp))
-                        fi
-                done
-                echo "Addition Employee and Wage: " $Emp " "$Addition
-                ;;
-        *)
-                echo "Invalid input"
-                ;;
-esac
-
-
+oneMonthWages=$(($totalHourForMonth*$wagePerHour))
+echo "Employee Wage For Month : " $oneMonthWages
