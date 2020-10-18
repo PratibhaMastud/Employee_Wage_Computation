5700f69587193b9a3f12e786f9499e49f7870881
PratibhaMastud <karandepratibha999@gmail.com>
2020-10-19 00:47:32 +0530

    Added:Calculate Wages till condition of Total Working Day.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,11 +1,19 @@
 #! /bin/bash
 #Author:Pratibha Mastud
-#Calculate Wage for Month
+#Calculate Wage till condition of total working hour
 
 wagePerHour=20
 workingDay=20
 fulldayHour=8
-totalHourForMonth=$(($workingDay*$fulldayHour))
+totalHourForMonth=100
+totalWages=0
+a=0
 
-oneMonthWages=$(($totalHourForMonth*$wagePerHour))
-echo "Employee Wage For Month : " $oneMonthWages
+for (( i=1; i<=20; i++ ))
+do	
+	perDayWages=$(($fulldayHour*$wagePerHour))
+	wageRespectiveDays=$(($perDayWages*i))
+	totalWages=$(($wageRespectiveDays+$totalWages))
+	a=$(($a+$i))
+	echo "Day : $i" "      $i-Day Wage = $wageRespectiveDays   " "  $a-Day total wage is $totalWages"
+done
