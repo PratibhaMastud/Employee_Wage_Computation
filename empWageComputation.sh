5753d16a677fe9c2c93ca9cc742aab9bab7a8478
PratibhaMastud <karandepratibha999@gmail.com>
2020-10-19 01:47:46 +0530

    Added:Store Daily Wage and Total Wage.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,15 +1,17 @@
 #! /bin/bash
 
-function workHour(){
-	var1=$i
-	
-	workingHour=$(($fullDayHour*$var1))
-
-	echo "Working Hour is = $workingHour"
-}
-fullDayHour=8
+wagePerHour=20
+workingDay=20
+fulldayHour=8
+totalWages=0
+a=0
 for (( i=1; i<=20; i++ ))
 do
-	echo "Working Day :" $i
-	workHour $i
+	perDayWages=$(($fulldayHour*$wagePerHour))
+	wageRespectiveDays=$(($perDayWages*i))
+	totalWages=$(($wageRespectiveDays+$totalWages))
+	a=$(($a+$i))
+	echo "$i-Daily Wage = $wageRespectiveDays   " 
 done
+	echo "Total Wage is $totalWages"
+
